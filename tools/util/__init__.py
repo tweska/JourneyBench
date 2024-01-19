450fe6c996987b546ac83b551b7ba88d4b1868c3
Kevin Nobel <git@2sk.nl>
2024-01-19 11:57:44 +0100

    add method to print a reconstructed journey

--- tools/util/__init__.py
+++ tools/util/__init__.py
@@ -1,1 +1,1 @@
-from .results import reconstruct_journey, check_journey
+from .results import reconstruct_journey, check_journey, print_reconstructed
--- tools/util/results.py
+++ tools/util/results.py
@@ -1,4 +1,4 @@
-
+import sys
 from typing import List, Union, Tuple
 
 from benchmark import Network, Journey, Conn, Path, JourneyPartType
@@ -25,6 +25,7 @@ def check_journey(reconstructed_journey, from_node_id, to_node_id, departure_tim
             prev_node = part.from_node_id
             next_node = part.to_node_id
             if part.departure_time < current_time:
+                print("Departure time is before current time!", file=sys.stderr)
                 return False, False, -1
             current_time = part.arrival_time
         else:
@@ -34,11 +35,25 @@ def check_journey(reconstructed_journey, from_node_id, to_node_id, departure_tim
             current_time += part.duration
 
         if prev_node != current_node:
+            print("Journey contains teleportation!", file=sys.stderr)
             return False, False, -1
         if next_node in visited_nodes:
+            print("Journey contains loop!", file=sys.stderr)
             logic = False
         current_node = next_node
 
     if current_node != to_node_id:
+        print("Journey does not reach destination!", file=sys.stderr)
         return False, False, -1
     return True, logic, current_time
+
+
+def print_reconstructed(reconstructed_journey):
+    parts = []
+    for part in reconstructed_journey:
+        if type(part) is Conn:
+            parts.append(f"Conn(trip={part.trip_id}, dep_node={part.from_node_id}, arr_node={part.to_node_id}, dep_time={part.departure_time}, arr_time={part.arrival_time})")
+        else:
+            assert type(part) is Path
+            parts.append(f"Path(dep_node={part.node_a_id}, arr_node={part.node_b_id}, duration={part.duration})")
+    print(" -> ".join(parts))
